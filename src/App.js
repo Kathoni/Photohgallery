6beded5a541dce92ab033c0ba5615f0c0b6042ae
Kathoni <thrusherkeys@gmail.com>
2024-08-08 23:16:05 +0300

    Fetching Api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import PhotoGrid from './Component/PhotoGrid'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1>Unsplash Photo Gallery</h1>
+      <PhotoGrid />
+    </div>
   )
 }
 
