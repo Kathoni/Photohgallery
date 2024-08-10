8a9fb4527cdeb06ba5b2c1264cd2b081d122b20d
Kathoni <thrusherkeys@gmail.com>
2024-08-10 11:07:06 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div>
       <h1
-      style={{
-        textAlign: 'center',
-        fontSize: '3rem',
-        margin: '2rem 0'
-        
-      }}>Unsplash Photo Gallery</h1>
+        style={{
+          textAlign: 'center',
+          fontSize: '3rem',
+          margin: '2rem 0'
+
+        }}>Unsplash Photo Gallery</h1>
       <PhotoGrid />
     </div>
   )
